5731180eac5913be5d012515ab37ad97d725571e
Florian Kinder <florian.kinder@fankserver.com>
2017-07-12 21:00:48 +0200

    Fixed upgrade system for 1.5.2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+set -e
 if [ ! -d /usr/src/app/node_modules ]
 then
-	git clone -b v1.5.x https://github.com/NodeBB/NodeBB.git /tmp/nodebb
-	cp -r /tmp/nodebb/* /usr/src/app/
-	rm /tmp/nodebb -rf
+	git clone -b v1.5.x https://github.com/NodeBB/NodeBB.git /usr/src/app/
 
 	npm install --production && npm cache clean --force
 	./nodebb setup $@
 else
+	git pull
+
 	./nodebb upgrade $@
 fi
 
